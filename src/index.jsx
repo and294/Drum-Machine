67b9b29d721864137303918ea0a4dbf5a6350bb6
Glitch (glitch-blank-react) <none>
2022-03-04 10:16:29 +0000

    👾🌮 Checkpoint ./src/app.jsx:32506642/7133 ./src/index.jsx:32506642/1952 ./src/styles/styles.css:32506642/6064 ./index.html:32506642/698

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app.jsx";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://vitejs.dev/guide/api-hmr.html
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
+
+var destination = document.getElementById("root")
+
+ReactDOM.render(
+    <App />, destination
+);
